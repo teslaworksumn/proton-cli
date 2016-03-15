112678063c4b544c7c540012d295f0268a3bfc9f
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-03-15 01:58:45 -0500

    HELLS YEAH I CAN MAKE A FOLDER

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -5,9 +5,9 @@ use std::fs;
 /// Creates a folder. The folder must not exist or must be empty.
 ///
 /// Impure.
-fn make_project_folder(root: &str) {
+pub fn make_project_folder(root: &str) {
     // Make the folder - ignore error.
-    fs::create_dir(root);
+    let _ = fs::create_dir(root);
 
     // Check that the folder is empty.
     if 0 != fs::read_dir(root).unwrap().count() {
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
-/// Executable for proton-cli
+/// Executable for proton_cli
 
-use proton_cli;
+extern crate proton_cli;
+
+use std::env;
 
 fn main() {
-    println!("Running main!");
+    let root = env::args().nth(1).unwrap();
+
+    proton_cli::init::make_project_folder(&root);
 }
