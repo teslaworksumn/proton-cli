f7248736ad6769699c499f68bb702e71aaea59be
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-03-17 17:28:24 -0500

    Cleaned up a bit

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,28 @@
 extern crate git2;
 extern crate rustc_serialize;
 
-pub mod init;
-pub mod project_types;
+use std::path::Path;
+use git2::Signature;
 
-// Re-export all error code into the top namespace
+mod init;
+mod project_types;
 mod error;
+
+// Re-export all error code into the this namespace
 pub use error::*;
+
+/// Initializes a new project at root. The root must either not exist, or must
+/// be an empty directory. This will
+///
+/// 1. Create the directory if it doesn't exist.
+/// 2. Create a Protonfile
+/// 3. Initialize a git repository and commit the protonfile.
+///
+/// Impure.
+pub fn initialize_project(root: &Path, signature: &Signature) -> Result<(), Error> {
+    init::make_project_folder(root)
+        .and_then(|_| init::make_protonfile(root))
+        .and_then(|_| init::make_repository(root))
+        .and_then(|repo| init::initial_commit(&repo, &signature))
+        .map(|_| ())
+}
--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,17 @@ use std::path::Path;
 
 use git2::{Signature, Time};
 
-use proton_cli::{init, Error};
-
 fn main() {
     let root_arg = env::args().nth(1).unwrap();
     let root = Path::new(&root_arg);
 
-    match init(root) {
-        Ok(_) => println!("Worked!"),
-        Err(e) => println!("{:?}", e),
-    }
-}
-
-fn init(root: &Path) -> Result<(), Error> {
     // TODO: Read a signature
     let time = Time::new(0, 0);
     let signature = Signature::new(
         "Proton Lights", "proton@teslaworks.net", &time).unwrap();
 
-    init::make_project_folder(root)
-        .and_then(|_| init::make_protonfile(root))
-        .and_then(|_| init::make_repository(root))
-        .and_then(|repo| init::initial_commit(&repo, &signature))
-        .map(|_| ())
+    match proton_cli::initialize_project(root, &signature) {
+        Ok(_) => println!("Worked!"),
+        Err(e) => println!("{:?}", e),
+    }
 }
