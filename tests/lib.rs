7efd7dd999e799b2fb221b4e7dbfa6347a47296a
Ramith Jayatilleka <ramith.jayatilleka@gmail.com>
2016-03-17 21:45:13 -0500

    Empty tests work

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,1 +1,20 @@
+#![feature(plugin)]
+#![plugin(stainless)]
+
 extern crate proton_cli;
+
+describe! initialize_project {
+    before_each {
+        println!("Before");
+    }
+    
+    after_each {
+        println!("After");
+    }
+
+    failing "works" {
+        println!("Hello");
+        panic!("Woah");
+    }
+
+}
