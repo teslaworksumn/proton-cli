914c171d7b52440c5dc8e6b4b22c7d2092a7df5e
Ryan Fredlund <rfredlund13@gmail.com>
2016-08-12 23:01:40 -0500

    Restructured into separate tests. 'is-working' tests complete?

--- tests/resection_sequence.rs
+++ tests/resection_sequence.rs
@@ -4,16 +4,18 @@ extern crate git2;
 
 mod common;
 
+use std::path::PathBuf;
+
 use common::setup;
 use common::rsa_keys::TestKey;
 use common::sequence_sections::{self, TestSeqSec};
 use proton_cli::project_types::{PermissionEnum};
 
 
-#[test]
-fn works_with_valid_inputs() {
+fn setup_resection<'a>() -> (PathBuf, &'a str) {
     let root = setup::setup_init_cd();
     let root_key_path = common::make_key_file(&root.path(), "root.pem", TestKey::RootKeyPem);
+    let user_key_path = common::make_key_file(&root.path(), "usera.pem", TestKey::GoodKeyPem);
     let name = "SequenceA";
     let sequence = setup::try_make_sequence(&root_key_path.as_path(), &name, "test_1sec.ogg");
     assert_eq!(sequence.num_sections, 1);
@@ -36,48 +38,27 @@ fn works_with_valid_inputs() {
         PermissionEnum::EditSeq,
         Some(name.to_owned()));
 
-    // Try to split 1 into 2
-    let seq_1_to_2 = proton_cli::resection_sequence(
-        &root_key_path,
-        &name,
-        2
-    ).expect("Error resectioning sequence");
+    (user_key_path, &name)
+}
+
+#[test]
+fn works_1_to_2() {
+    let (user_key_path, name) = setup_resection();
+
+    let seq_1_to_2 = proton_cli::resection_sequence(&user_key_path.as_path(), name, 2)
+        .expect("Error resectioning sequence");
     assert_eq!(seq_1_to_2.num_sections, 2);
     let seq_sec_1_of_2 = seq_1_to_2.get_section(1).expect("Error retrieving sequence section");
     let seq_sec_2_of_2 = seq_1_to_2.get_section(2).expect("Error retrieving sequence section");
     assert_eq!(seq_sec_1_of_2.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of2));
     assert_eq!(seq_sec_2_of_2.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good2of2));
+}
 
-    // Try to split 2 into 3
-    let seq_2_to_3 = proton_cli::resection_sequence(
-        &root_key_path,
-        &name,
-        3
-    ).expect("Error resectioning sequence");
-    assert_eq!(seq_2_to_3.num_sections, 3);
-    let seq_sec_1_of_3 = seq_2_to_3.get_section(1).expect("Error retrieving sequence section");
-    let seq_sec_2_of_3 = seq_2_to_3.get_section(2).expect("Error retrieving sequence section");
-    let seq_sec_3_of_3 = seq_2_to_3.get_section(3).expect("Error retrieving sequence section");
-    assert_eq!(seq_sec_1_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of3));
-    assert_eq!(seq_sec_2_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good2of3));
-    assert_eq!(seq_sec_3_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good3of3));
-
-    // Try to merge 3 into 1
-    let seq_3_to_1 = proton_cli::resection_sequence(
-        &root_key_path,
-        &name,
-        1
-    ).expect("Error resectioning sequence");
-    assert_eq!(seq_3_to_1.num_sections, 1);
-    let seq_sec_1_of_1 = seq_3_to_1.get_section(1).expect("Error retrieving sequence section");
-    assert_eq!(seq_sec_1_of_1.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of1));
-
-    // Try to split 1 into 3
-    let seq_1_to_3 = proton_cli::resection_sequence(
-        &root_key_path,
-        &name,
-        3
-    ).expect("Error resectioning sequence");
+#[test]
+fn works_1_to_3() {
+    let (user_key_path, name) = setup_resection();
+    let seq_1_to_3 = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 3)
+        .expect("Error resectioning sequence");
     assert_eq!(seq_1_to_3.num_sections, 3);
     let seq_sec_1_of_3 = seq_1_to_3.get_section(1).expect("Error retrieving sequence section");
     let seq_sec_2_of_3 = seq_1_to_3.get_section(2).expect("Error retrieving sequence section");
@@ -85,4 +66,49 @@ fn works_with_valid_inputs() {
     assert_eq!(seq_sec_1_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of3));
     assert_eq!(seq_sec_2_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good2of3));
     assert_eq!(seq_sec_3_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good3of3));
+}
+
+#[test]
+fn works_2_to_3() {
+    let (user_key_path, name) = setup_resection();
+    // Initially split into 2 before trying to split further
+    let _ = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 2)
+        .expect("Error doing initial resequence");
+    let seq_2_to_3 = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 3)
+        .expect("Error resectioning sequence");
+    assert_eq!(seq_2_to_3.num_sections, 3);
+    let seq_sec_1_of_3 = seq_2_to_3.get_section(1).expect("Error retrieving sequence section");
+    let seq_sec_2_of_3 = seq_2_to_3.get_section(2).expect("Error retrieving sequence section");
+    let seq_sec_3_of_3 = seq_2_to_3.get_section(3).expect("Error retrieving sequence section");
+    assert_eq!(seq_sec_1_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of3));
+    assert_eq!(seq_sec_2_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good2of3));
+    assert_eq!(seq_sec_3_of_3.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good3of3));
+}
+
+#[test]
+fn works_3_to_2() {
+    let (user_key_path, name) = setup_resection();
+    // Initially split into 3 before trying to merge back
+    let _ = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 3)
+        .expect("Error doing initial resequence");
+    let seq_3_to_2 = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 2)
+        .expect("Error resectioning sequence");
+    assert_eq!(seq_3_to_2.num_sections, 2);
+    let seq_sec_1_of_2 = seq_3_to_2.get_section(1).expect("Error retrieving sequence section");
+    let seq_sec_2_of_2 = seq_3_to_2.get_section(2).expect("Error retrieving sequence section");
+    assert_eq!(seq_sec_1_of_2.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of2));
+    assert_eq!(seq_sec_2_of_2.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good2of2));
+}
+
+#[test]
+fn works_3_to_1() {
+    let (user_key_path, name) = setup_resection();
+    // Initially split into 3 before trying to merge back
+    let _ = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 3)
+        .expect("Error doing initial resequence");
+    let seq_3_to_1 = proton_cli::resection_sequence(&user_key_path.as_path(), &name, 1)
+        .expect("Error resectioning sequence");
+    assert_eq!(seq_3_to_1.num_sections, 1);
+    let seq_sec_1_of_1 = seq_3_to_1.get_section(1).expect("Error retrieving sequence section");
+    assert_eq!(seq_sec_1_of_1.data, sequence_sections::get_test_seq_sec(TestSeqSec::Good1of1));
 }
