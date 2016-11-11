879239b17544e67a6886a804c58559c4c857958e
Ryan Fredlund <rfredlund13@gmail.com>
2016-11-11 17:05:40 -0600

    get_layout implemented, not tested

--- src/dao/layout_dao_postgres.rs
+++ src/dao/layout_dao_postgres.rs
@@ -29,7 +29,7 @@ impl LayoutDao for LayoutDaoPostgres {
     }
 
     fn get_layout(&self, layout_id: u32) -> Result<Layout, Error> {
-        let query = "SELECT * FROM layouts WHERE layoutid = $1";
+        let query = "SELECT name, fixtures FROM layouts WHERE layoutid = $1";
         let lid = layout_id as i32;
         let results = try!(
             self.conn.query(query, &[&lid])
@@ -37,7 +37,15 @@ impl LayoutDao for LayoutDaoPostgres {
         match results.len() {
             0 => Err(Error::LayoutNotFound(layout_id)),
             1 => {
-                Err(Error::TodoErr)
+                let row = results.get(0);
+                let name: String = row.get(0);
+                let fixtures: Vec<i32> = row.get(1);
+                let fixtures_u32 = fixtures.iter().map(|x| x.to_owned() as u32).collect();
+                Ok(Layout {
+                    layout_id: layout_id,
+                    name: name,
+                    fixtures: fixtures_u32
+                })
             },
             x => Err(Error::InvalidNumResults(x)),
         }
