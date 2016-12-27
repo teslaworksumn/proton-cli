c2f84242f4ab9480e07d42abd454bf7949d4f4ec
Ryan Fredlund <rfredlund13@gmail.com>
2016-12-26 18:37:27 -0600

    Removed unnecessary test

--- tests/get_layoutid.rs
+++ tests/get_layoutid.rs
@@ -3,7 +3,6 @@ extern crate proton_cli;
 mod dao;
 
 use proton_cli::project_types::Project;
-use proton_cli::error::Error;
 
 
 #[test]
@@ -34,23 +33,5 @@ fn proper_id_returned_with_existing_name() {
 fn fails_if_name_invalid() {
     let project_name = "_*($%";
     let project_dao = dao::ProjectDaoTesting::new();
-    let _ = proton_cli::get_layout_id(&project_dao, project_name).expect("Error getting layout id");
-}
-
-#[test]
-#[should_panic(expected = "ProjectNotFound")]
-fn fails_if_name_not_found() {
-    let project_name = "Name";
-    let mut project_dao = dao::ProjectDaoTesting::new();
-    project_dao.get_project_fn = Box::new(|name: String| {
-        match name.as_ref() {
-            "OtherName" => Ok(Project {
-                name: name,
-                playlist: Vec::new(),
-                layout_id: 1
-            }),
-            _ => Err(Error::ProjectNotFound(name))
-        }
-    });
     let _ = proton_cli::get_layout_id(&project_dao, project_name).expect("Error getting layout id");
 }
